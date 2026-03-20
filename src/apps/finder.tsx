e7dc58ed3f82582e632278106e25984a62874afd
Krixsick <linusgao123@gmail.com>
2026-03-20 19:11:49 -0400

    Added so you can open up finder and switch between the two options present

--- src/apps/finder.tsx
+++ src/apps/finder.tsx
@@ -1,3 +1,40 @@
+import { useState } from "react";
+
 export function FinderContent() {
-  return <div>wadwad</div>;
+  const [fileOptions, setFileOptions] = useState<Record<string, boolean>>({
+    Desktop: true,
+    Downloads: false,
+  });
+  return (
+    <div className="w-full h-full flex">
+      {/* Left Side*/}
+      <div className="flex flex-col p-2 bg-[#e8e8e8] w-[20%] h-full border-l">
+        <p className="font-mono text-light opacity-60 text-[gray-300] text-black text-[clamp(8px,2vw,10px)]">
+          Favorites
+        </p>
+        {Object.entries(fileOptions).map(([option, state]) => {
+          return (
+            <button
+              onClick={() =>
+                setFileOptions((prev) => {
+                  const updated: Record<string, boolean> = {};
+                  for (const key of Object.keys(prev)) {
+                    updated[key] = key === option;
+                  }
+                  return updated;
+                })
+              }
+              key={option}
+              className={`p-1 rounded-xl w-full text-left font-mono text-light text-black text-[clamp(8px,2vw,14px)] cursor-pointer transition-colors active:bg-white/60 ${state ? "bg-black/10" : ""}`}
+            >
+              {option}
+            </button>
+          );
+        })}
+      </div>
+      <div className="bg-white w-[80%] h-full">
+        {fileOptions["Desktop"] === true ? <div></div> : <div></div>}
+      </div>
+    </div>
+  );
 }
